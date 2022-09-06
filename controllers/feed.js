48e0e059f1bf74699085d4969d317bbe445e5396
harsh-haria <harshharia1234@gmail.com>
2022-09-06 15:59:48 +0530

    Basic setup and validation of input

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,41 @@
-exports.getPosts = (req,res,next) => {
-    res.status(200).json({
-      posts: [{ title: "first post", content: "This is the first post" }],
-    });
+const { validationResult } = require("express-validator");
+
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "first post",
+        content: "This is the first post",
+        imageUrl: "images/ducky.jpg",
+        creator: {
+          name: "Harsh",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
 };
 
-exports.createPost = (req,res,next) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    //save data in DB
-    res.status(201).json({
-      message: "Post Created Successfully",
-      post: { id: new Date().toISOString(), title: title, content: content },
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation Failed! Entered Data is Incorrect :(",
+      errors: errors.array(),
     });
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  //save data in DB
+  res.status(201).json({
+    message: "Post Created Successfully",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "HarshHaria" },
+      createdAt: new Date(),
+    },
+  });
 };

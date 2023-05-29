990c903e7e39c392f2cc9a0ca2b5a61996fb2c06
smartshoper <harshharia1234@gmail.com>
2023-05-29 23:38:07 +0530

    Validations for graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,21 @@
+const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
 module.exports = {
     createUser: async ({ userInput }, req) => {
+        let errors = [];
+        if (!validator.isEmail(userInput.email)) {
+            errors.push({ message: 'E-Mail is invalid.' })
+        }
+        if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+            errors.push({ message: 'Password too short!' })
+        }
+        if (errors.length > 0) {
+            const error = new Error('Invalid inputs');
+            throw error;
+        }
         const exisitngUser = await User.findOne({ email: userInput.email });
         if (exisitngUser) {
             const error = new Error('User exists already!');
